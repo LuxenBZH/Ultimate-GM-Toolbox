a2ec932d9598b39bbe00f7ffe659cd4c15ac5e90
Luxen <luxen.zh@gmail.com>
2022-09-11 20:06:15 +0200

    Added races visual resources mapping

--- Mods/lx_ugmtools_b331010b-6678-4373-8cc4-8f004ab0bf36/Story/RawFiles/Lua/Shared/_InitShared.lua
+++ Mods/lx_ugmtools_b331010b-6678-4373-8cc4-8f004ab0bf36/Story/RawFiles/Lua/Shared/_InitShared.lua
@@ -1,5 +1,6 @@
 Ext.Require("Shared/Helpers.lua")
 Ext.Require("Shared/StatusManager.lua")
 Ext.Require("Shared/VisualResources.lua")
+Ext.Require("Shared/Helpers/VisualResources.lua")
 
 Classes = {}
